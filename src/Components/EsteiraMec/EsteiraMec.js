d10a5758f597172906140963708c181e1f35dea5
Otavio <82684258+otavio39@users.noreply.github.com>
2024-08-15 13:37:16 +0000

    build

--- src/Components/EsteiraMec/EsteiraMec.js
+++ src/Components/EsteiraMec/EsteiraMec.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './EsteiraMec.module.css';
 
 import Adoorei from '../../Images/Esteira/adoorei.svg';
@@ -7,14 +7,35 @@ import Meta from '../../Images/Esteira/meta.svg';
 import Shopify from '../../Images/Esteira/shopify.svg';
 import Tiktok from '../../Images/Esteira/tiktok.svg';
 import Unicodrop from '../../Images/Esteira/unicodrop.svg';
-import Utmify from '../../Images/Esteira/utmify.svg';
+import Utmify from '../../Images/Esteira/utmify2.svg';
 import Vega from '../../Images/Esteira/vega.svg';
 import Voxuy from '../../Images/Esteira/voxuy.svg';
 
 const EsteiraLogos = () => {
+  const esteiraRef = useRef(null);
+
+  useEffect(() => {
+    const handleScrollToCenter = () => {
+      if (esteiraRef.current) {
+        const scrollWidth = esteiraRef.current.scrollWidth;
+        const clientWidth = esteiraRef.current.clientWidth;
+
+        if (scrollWidth > clientWidth) {
+          esteiraRef.current.scrollLeft = (scrollWidth - clientWidth) / 2;
+        }
+      }
+    };
+
+    // Adiciona um pequeno atraso para garantir que o layout esteja completo antes de ajustar o scroll
+    const timeoutId = setTimeout(handleScrollToCenter, 500);
+
+    // Cleanup function to clear timeout if the component unmounts
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
-    <div className={styles.Esteira}>
-        <div className={styles.Slider}>
+    <div className={styles.Esteira} ref={esteiraRef}>
+        <div>
             <img src={Unicodrop} draggable="false" alt="Unicodrop" />
             <img src={Googleads} draggable="false" alt="Googleads" />
             <img src={Vega} draggable="false" alt="Vega" />
@@ -23,16 +44,7 @@ const EsteiraLogos = () => {
             <img src={Adoorei} draggable="false" alt="Adoorei" />
             <img src={Meta} draggable="false" alt="Meta" />
             <img src={Tiktok} draggable="false" alt="Tiktok" />
-            <img src={Utmify} draggable="false" alt="Utmify"/>
-            <img src={Unicodrop} draggable="false" alt="Unicodrop" />
-            <img src={Googleads} draggable="false" alt="Googleads" />
-            <img src={Vega} draggable="false" alt="Vega" />
-            <img src={Voxuy} draggable="false" alt="Voxuy" />
-            <img src={Shopify} draggable="false" alt="Shopify" />
-            <img src={Adoorei} draggable="false" alt="Adoorei" />
-            <img src={Meta} draggable="false" alt="Meta" />
-            <img src={Tiktok} draggable="false" alt="Tiktok" />
-            <img src={Utmify} draggable="false" alt="Utmify"/>
+            <img src={Utmify} draggable="false" alt="Utmify" style={{ marginRight: '20px' }}/>
         </div>
     </div>
   );
