7433ec9b7930254e0edecd58063e0aa5190c9d05
Otavio <82684258+otavio39@users.noreply.github.com>
2024-08-15 13:42:49 +0000

    build

--- src/Components/EsteiraMec/EsteiraMec.js
+++ src/Components/EsteiraMec/EsteiraMec.js
@@ -44,7 +44,16 @@ const EsteiraLogos = () => {
             <img src={Adoorei} draggable="false" alt="Adoorei" />
             <img src={Meta} draggable="false" alt="Meta" />
             <img src={Tiktok} draggable="false" alt="Tiktok" />
-            <img src={Utmify} draggable="false" alt="Utmify" style={{ marginRight: '20px' }}/>
+            <img src={Utmify} draggable="false" alt="Utmify"/>
+            <img src={Unicodrop} draggable="false" alt="Unicodrop" />
+            <img src={Googleads} draggable="false" alt="Googleads" />
+            <img src={Vega} draggable="false" alt="Vega" />
+            <img src={Voxuy} draggable="false" alt="Voxuy" />
+            <img src={Shopify} draggable="false" alt="Shopify" />
+            <img src={Adoorei} draggable="false" alt="Adoorei" />
+            <img src={Meta} draggable="false" alt="Meta" />
+            <img src={Tiktok} draggable="false" alt="Tiktok" />
+            <img src={Utmify} draggable="false" alt="Utmify"/>
         </div>
     </div>
   );
