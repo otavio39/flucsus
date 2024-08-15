e3de917c113991dd7702f40803d90bcc5309fafc
Otavio <82684258+otavio39@users.noreply.github.com>
2024-08-15 05:11:50 +0000

    build

--- src/Components/Esteira/Esteira.js
+++ src/Components/Esteira/Esteira.js
@@ -15,31 +15,31 @@ const EsteiraLogos = () => {
   return (
     <div className={styles.Esteira}>
       <div>
-        <h3 className={styles.titleblack}>Facilitamos seu crescimento com nossos <strong style={{ fontWeight: '900' }}>parceiros e integrações</strong></h3>
+        <h3 className={styles.titleblack}>Empresas que confiam</h3>
       </div>
       <div className={styles.EsteiraContainer}>
         <div className={styles.EsteiraContent}>
             <div>
-              <img src={Shopify} draggable="false" alt="Shopify" />
-              <img src={Unicodrop} draggable="false" alt="Unicodrop" />
-              <img src={Voxuy} draggable="false" alt="Voxuy" />
-              <img src={Adoorei} draggable="false" alt="Adoorei" />
-              <img src={Googleads} draggable="false" alt="Googleads" />
-              <img src={Meta} draggable="false" alt="Meta" />
-              <img src={Tiktok} draggable="false" alt="Tiktok" />
-              <img src={Utmify} draggable="false" alt="Utmify" />
-              <img src={Vega} draggable="false" alt="Vega" />
+                <img src={Shopify} draggable="false" alt="Shopify" />
+                <img src={Unicodrop} draggable="false" alt="Unicodrop" />
+                <img src={Voxuy} draggable="false" alt="Voxuy" />
+                <img src={Adoorei} draggable="false" alt="Adoorei" />
+                <img src={Googleads} draggable="false" alt="Googleads" />
+                <img src={Meta} draggable="false" alt="Meta" />
+                <img src={Tiktok} draggable="false" alt="Tiktok" />
+                <img src={Utmify} draggable="false" alt="Utmify" />
+                <img src={Vega} draggable="false" alt="Vega" />
             </div>
             <div>
-              <img src={Shopify} draggable="false" alt="Shopify" />
-              <img src={Unicodrop} draggable="false" alt="Unicodrop" />
-              <img src={Voxuy} draggable="false" alt="Voxuy" />
-              <img src={Adoorei} draggable="false" alt="Adoorei" />
-              <img src={Googleads} draggable="false" alt="Googleads" />
-              <img src={Meta} draggable="false" alt="Meta" />
-              <img src={Tiktok} draggable="false" alt="Tiktok" />
-              <img src={Utmify} draggable="false" alt="Utmify" />
-              <img src={Vega} draggable="false" alt="Vega" />
+                <img src={Shopify} draggable="false" alt="Shopify" />
+                <img src={Unicodrop} draggable="false" alt="Unicodrop" />
+                <img src={Voxuy} draggable="false" alt="Voxuy" />
+                <img src={Adoorei} draggable="false" alt="Adoorei" />
+                <img src={Googleads} draggable="false" alt="Googleads" />
+                <img src={Meta} draggable="false" alt="Meta" />
+                <img src={Tiktok} draggable="false" alt="Tiktok" />
+                <img src={Utmify} draggable="false" alt="Utmify" />
+                <img src={Vega} draggable="false" alt="Vega" />
             </div>
         </div>
       </div>
@@ -48,3 +48,4 @@ const EsteiraLogos = () => {
 }
 
 export default EsteiraLogos;
+
